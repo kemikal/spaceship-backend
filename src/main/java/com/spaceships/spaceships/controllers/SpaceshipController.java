1fc0f99218948d7cd6330250d417828202e03075
kemikal <janne@kemikal.org>
2024-04-26 09:35:33 +0200

    fixes

--- src/main/java/com/spaceships/spaceships/controllers/SpaceshipController.java
+++ src/main/java/com/spaceships/spaceships/controllers/SpaceshipController.java
@@ -7,6 +7,7 @@ import com.spaceships.spaceships.services.SpaceshipService;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class SpaceshipController {
     
